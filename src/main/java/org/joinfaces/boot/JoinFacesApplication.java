c17689930544eb11b4b7ae93779e85ac1539b62b
Philmasteryeah <p.schulz89@gmx.de>
2017-03-10 21:48:33 +0100

    #renamed test

--- src/main/java/org/joinfaces/boot/JoinFacesApplication.java
+++ src/main/java/org/joinfaces/boot/JoinFacesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JoinFacesExampleApplication {
+public class JoinFacesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JoinFacesExampleApplication.class, args);
+		SpringApplication.run(JoinFacesApplication.class, args);
 	}
 
 }
--- src/test/java/org/joinfaces/boot/JoinFacesApplicationTest.java
+++ src/test/java/org/joinfaces/boot/JoinFacesApplicationTest.java
@@ -2,13 +2,12 @@ package org.joinfaces.boot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = JoinFacesExampleApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class JoinFacesExampleApplicationIT {
+@SpringBootTest(classes = JoinFacesApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class JoinFacesApplicationTest {
 
 	@Test
 	public void contextLoads() {
