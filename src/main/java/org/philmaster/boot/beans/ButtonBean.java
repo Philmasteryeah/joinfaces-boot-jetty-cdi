328ff2a0477f08ae52a3ee2fdd0cc13922eaf2f3
Philmasteryeah <p.schulz89@gmx.de>
2017-03-16 23:09:40 +0100

    index page updated

---
+++ src/main/java/org/philmaster/boot/beans/ButtonBean.java
@@ -0,0 +1,21 @@
+package org.philmaster.boot.beans;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class ButtonBean {
+     
+    public void buttonAction(ActionEvent actionEvent) {
+        addMessage("Welcome to Primefaces!!");
+    }
+     
+    public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
