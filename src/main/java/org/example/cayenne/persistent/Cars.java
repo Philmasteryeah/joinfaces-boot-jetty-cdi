a9561a7e80364d16741d7ad954544822368da304
Philmasteryeah <p.schulz89@gmx.de>
2017-03-25 22:59:40 +0100

    # cayenne orm is working now

---
+++ src/main/java/org/example/cayenne/persistent/Cars.java
@@ -0,0 +1,9 @@
+package org.example.cayenne.persistent;
+
+import org.example.cayenne.persistent.auto._Cars;
+
+public class Cars extends _Cars {
+
+    private static final long serialVersionUID = 1L;
+
+}
