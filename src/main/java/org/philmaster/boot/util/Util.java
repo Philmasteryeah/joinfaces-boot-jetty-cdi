9d7f26eacc620a4afe034aa1d96efcdb52bfc0a4
Philmasteryeah <p.schulz89@gmx.de>
2017-11-02 19:26:45 +0100

    TODO java.nio

--- src/main/java/org/philmaster/boot/util/Util.java
+++ src/main/java/org/philmaster/boot/util/Util.java
@@ -9,10 +9,12 @@ import org.apache.commons.io.IOUtils;
 
 public class Util {
 
+    // TODO this should be java.nio
     public static File writeStreamToTempFile(InputStream in, String filename) throws IOException {
 	if (in == null || filename == null)
 	    return null;
 	final String[] filenameParts = filename.split("\\.");
+	// Path tmpFile = Files.createTempFile(dir, prefix, suffix, attrs)
 	final File tempFile = File.createTempFile(filenameParts[0], filenameParts[1]);
 	tempFile.deleteOnExit();
 	try (FileOutputStream out = new FileOutputStream(tempFile)) {
--- src/main/java/org/philmaster/boot/views/UploadView.java
+++ src/main/java/org/philmaster/boot/views/UploadView.java
@@ -1,15 +1,10 @@
 package org.philmaster.boot.views;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.stream.Collectors;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
