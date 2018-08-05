17b933d781af300304f2e42fee8b8ff1362e98cb
Philmasteryeah <p.schulz89@gmx.de>
2018-08-05 21:19:42 +0200

    # updated jsf, + rest route

--- src/main/java/org/philmaster/boot/controller/FoodController.java
+++ src/main/java/org/philmaster/boot/controller/FoodController.java
@@ -1,5 +1,8 @@
 package org.philmaster.boot.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.philmaster.boot.model.Meal;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,9 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FoodController {
 
-	@RequestMapping(path="/test/meal", method = RequestMethod.GET)
-	public ResponseEntity<Meal> listUser(){
+	@RequestMapping(path = "/test/meal", method = RequestMethod.GET)
+	public ResponseEntity<Meal> getTestMeal() {
 		return new ResponseEntity<Meal>(Meal.getTestMeal(), HttpStatus.OK);
+	}
+
+	@RequestMapping(path = "/test/meals", method = RequestMethod.GET)
+	public ResponseEntity<List<Meal>> getTestMeals() {
+		return new ResponseEntity<List<Meal>>(Arrays.asList(Meal.getTestMeal(), Meal.getTestMeal()), HttpStatus.OK);
 	}
 
 }
