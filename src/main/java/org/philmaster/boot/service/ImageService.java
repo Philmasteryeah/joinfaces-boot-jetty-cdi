d881d8994c7a977e3727cccba233c013201ddc7e
Philmasteryeah <p.schulz89@gmx.de>
2018-11-04 19:26:12 +0100

    # pom cleanup

--- src/main/java/org/philmaster/boot/service/ImageService.java
+++ src/main/java/org/philmaster/boot/service/ImageService.java
@@ -8,7 +8,6 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
