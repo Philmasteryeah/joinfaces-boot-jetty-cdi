8ff7c4a3c6ed39893693389d452bf9ec121760a6
Philmasteryeah <p.schulz89@gmx.de>
2018-11-06 00:40:43 +0100

    # cleanup and prepared datalistcolumntemplate

--- src/main/java/org/philmaster/boot/util/Util.java
+++ src/main/java/org/philmaster/boot/util/Util.java
@@ -5,7 +5,6 @@ import java.lang.reflect.Field;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
