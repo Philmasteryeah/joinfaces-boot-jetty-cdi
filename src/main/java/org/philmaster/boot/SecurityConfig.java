1fdc4246119ec8462ca6e9cfc0373e02fa7d1acc
Philmasteryeah <p.schulz89@gmx.de>
2018-12-27 02:01:45 +0100

    # formatting

--- src/main/java/org/philmaster/boot/SecurityConfig.java
+++ src/main/java/org/philmaster/boot/SecurityConfig.java
@@ -5,20 +5,13 @@ import javax.inject.Inject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.philmaster.boot.service.DatabaseService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.User.UserBuilder;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class SecurityConfig {
@@ -52,7 +45,7 @@ public class SecurityConfig {
 			http.antMatcher("/rest/**")
 					.authorizeRequests()
 					.anyRequest()
-					.authenticated()
+					.hasRole("ADMIN")
 				.and()
 					.httpBasic();
 
